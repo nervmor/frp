874e741f9158d6ad448a4d43d2bb90a195d05fe2
nervmor <nervmor@126.com>
2024-08-23 03:28:24 +0000

    remove package actions which is not required

--- package.sh
+++ package.sh
@@ -17,8 +17,8 @@ make -f ./Makefile.cross-compiles
 rm -rf ./release/packages
 mkdir -p ./release/packages
 
-os_all='linux windows darwin freebsd android'
-arch_all='386 amd64 arm arm64 mips64 mips64le mips mipsle riscv64'
+os_all='linux windows darwin'
+arch_all='amd64'
 extra_all='_ hf'
 
 cd ./release
